9f8de71bdc3a7ab04489630fec4c78e44848fbe6
Josue Rosario <josue.rosario@escolar509.sp.senai.br>
2024-04-30 22:14:52 -0300

    aula 2 de webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import 'bootstrap';
+import './styles.scss'
+import * as bootstrap from 'bootstrap'
 import geradorConselho from './GeradorConselho';
 console.log(geradorConselho())
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,45 @@ const path = require('path')
 
 module.exports = {
     mode: 'development',
+    
     entry: path.resolve(__dirname,'src/index.js'),
+    
     output:{
         path: path.resolve(__dirname,'dist'),
         filename:'main.js',
     },   
+
+    module: {
+        rules: [
+          {
+            test: /\.(scss)$/,
+            use: [
+              {
+                // Adds CSS to the DOM by injecting a `<style>` tag
+                loader: 'style-loader'
+              },
+              {
+                // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                loader: 'css-loader'
+              },
+              {
+                // Loader for webpack to process CSS with PostCSS
+                loader: 'postcss-loader',
+                options: {
+                  postcssOptions: {
+                    plugins: () => [
+                      autoprefixer
+                    ]
+                  }
+                }
+              },
+              {
+                // Loads a SASS/SCSS file and compiles it to CSS
+                loader: 'sass-loader'
+              }
+            ]
+          }
+        ]
+      }
+    
 }
